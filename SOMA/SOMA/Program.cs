c3074846ee74fbb5739808f43420339c3f44ee0e
Marcos Vinicius <kinhoviny2818@gmail.com>
2022-10-24 17:49:29 -0300

    Soma

---
+++ SOMA/SOMA/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SOMA;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        int INDICE = 13;
+        int SOMA = 0, K = 0;
+
+        for (int i1 = 0; K < INDICE; i1++)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+            Console.WriteLine(SOMA);
+        }
+        Console.WriteLine("Valor final é = " + SOMA);
+    }
+}
